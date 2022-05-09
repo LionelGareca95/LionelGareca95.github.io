4e93cb12da833ad4978dd5ff12501e0821d3a978
Lionel Gareca <lionelgareca@gmail.com>
2022-05-09 15:28:53 -0300

    Añadiendo Scrolling Top y Barra Scroll

--- src/js/scrollReveal.js
+++ src/js/scrollReveal.js
@@ -17,4 +17,31 @@ window.sr = ScrollReveal();
         distance: '-300px'
     });
 
-    
+    /* Scrolling top*/
+
+        addEventListener('DOMContentLoaded', () => {
+        const boton_ir_arriba = document.querySelector('#ir_arriba_boton')
+        const barra_indicador = document.querySelector('.indicador_scroll')
+
+        const obtener_pixeles_inicio = () => document.documentElement.scrollTop || document.body.scrollTop
+
+        const irArriba = () => {
+            if (obtener_pixeles_inicio() > 0) {
+                scrollTo(0,0)
+            }
+        }
+
+        const indicador_scroll = () => {
+            if(obtener_pixeles_inicio() > 50) {
+                boton_ir_arriba.className = 'mostrar'
+            } else {
+                boton_ir_arriba.className = 'ocultar'
+            }
+            let alto = document.documentElement.scrollHeight - document.documentElement.clientHeight
+            let avance_scroll = (obtener_pixeles_inicio() / alto ) * 100
+            barra_indicador.style.width = `${avance_scroll}%`
+        }
+
+        boton_ir_arriba.addEventListener('click', irArriba)
+        window.addEventListener('scroll', indicador_scroll)
+    })
