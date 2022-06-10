4e3e85a18ed22631fcd533ad037911484ed7f086
LionelGareca95 <lionelgareca@gmail.com>
2022-06-10 13:26:15 -0300

    Va funcionando menu burguer

--- src/js/menuHamburguesa.js
+++ src/js/menuHamburguesa.js
@@ -4,9 +4,9 @@ const links = document.querySelectorAll('#nav li')
 
 hamburguesa.addEventListener('click', () => {
     nav.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade")
-    })
+    // links.forEach(link => {
+    //     link.classList.toggle("fade")
+    // })
 })
 
 
